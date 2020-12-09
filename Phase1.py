8c047f1b613226848c21f82ad9e88afc7174e32d
Lucas-CG <lucas.gomes@poli.ufrj.br>
2020-12-08 21:17:54 -0300

    Added phase1 parallel execution script

--- Phase1.py
+++ Phase1.py
@@ -1,35 +1,38 @@
 import subprocess
 from multiprocessing import Pool
 
-def runABC(runid):
+def runABC():
     result = subprocess.check_output(['python3', 'RunABCExperiments.py'])
 
-def runACO(runid):
+def runACO():
     result = subprocess.check_output(['python3', 'RunACOExperiments.py'])
 
-def runAGA(runid):
+def runAGA():
     result = subprocess.check_output(['python3', 'RunAGAExperiments.py'])
 
-def runDE(runid):
+def runDE():
     result = subprocess.check_output(['python3', 'RunDEExperiments.py'])
 
-def runGA(runid):
+def runGA():
     result = subprocess.check_output(['python3', 'RunGAExperiments.py'])
 
-def runPSO(runid):
+def runPSO():
     result = subprocess.check_output(['python3', 'RunPSOExperiments.py'])
 
-def runRegPSO(runid):
+def runRegPSO():
     result = subprocess.check_output(['python3', 'RunRegPSOExperiments.py'])
 
-def runSSO(runid):
+def runSSO():
     result = subprocess.check_output(['python3', 'RunSSOExperiments.py'])
 
+def doFunc(func):
+    func()
 
 if __name__ == "__main__":
 
-    runs = 25
+    runs = 8
     p = Pool(runs)
-    outputs = p.imap(runACO, range(runs))
+    funList = [runABC, runACO, runAGA, runDE, runGA, runPSO, runRegPSO, runSSO]
+    outputs = p.imap(doFunc, funList)
     for output in outputs:
         print(output)
